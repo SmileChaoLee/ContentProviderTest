70bab868921d00b1594957ad860c278baed1f405
chaolee <leechao1959@gmail.com>
2017-10-24 08:36:58 -0400

    from target level from 26 to 23

---
+++ app/src/main/java/com/smile/contentprovidertest/EmployeeContentProvider.java
@@ -0,0 +1,214 @@
+package com.smile.contentprovidertest;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import java.util.HashMap;
+
+public class EmployeeContentProvider extends ContentProvider {
+
+    static final String providerName = new String("com.smile.provider.SmileCompany");
+    static final String URL = new String("content://"+providerName+"/employees");
+    static final Uri contentURI = Uri.parse(URL);
+
+    static final String employeeId = new String("_id");
+    static final String employeeName = new String("name");
+    static final String employeePhone = new String("phone");
+
+    private static HashMap<String,String> employeesMap;
+
+    final static int EMPLOYEES = 1;
+    final static int EMPLOYEE_ID = 2;
+
+    final static UriMatcher uriMatcher;
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(providerName,"employees",EMPLOYEES);
+        uriMatcher.addURI(providerName,"employees/#",EMPLOYEE_ID);
+    }
+    /**
+     * Database specific constant declarations
+     */
+    private SQLiteDatabase employeeDb;
+    static final String databaseName = "SmileCompany";
+    static final String tableName = "employees";
+    static final int databaseVersion = 1;
+    static final String CREATE_DB_TABLE =
+            " CREATE TABLE " + tableName +
+                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " name TEXT NOT NULL, " +
+                    " phone TEXT NOT NULL);";
+
+    /**
+     * Helper class that actually creates and manages
+     * the provider's underlying data repository.
+     */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context) {
+            super(context,databaseName,null,databaseVersion);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DB_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + tableName);
+            onCreate(db);
+
+        }
+    }
+    // end of SQLiteDatabase creating
+
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+        /**
+         * Create a write able database which will trigger its
+         * creation if it doesn't already exist.
+         */
+        employeeDb = dbHelper.getWritableDatabase();
+        if (employeeDb == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int count = 0;
+
+        switch (uriMatcher.match(uri)){
+            case EMPLOYEES:
+                count = employeeDb.delete(tableName, selection, selectionArgs);
+                break;
+
+            case EMPLOYEE_ID:
+                String id = uri.getPathSegments().get(1);
+                count = employeeDb.delete( tableName,employeeId +  " = " + id +
+                        (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)){
+            /**
+             * Get all employees  records
+             */
+            case EMPLOYEES:
+                return "vnd.android.cursor.dir/vnd.smile.employees";
+
+            /**
+             * Get a particular employee
+             */
+            case EMPLOYEE_ID:
+                return "vnd.android.cursor.item/vnd.smile.employees";
+
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        /**
+         *  Add a new employee record
+         */
+        long rowId = employeeDb.insert(tableName, "", values);
+        /**
+         * If record is added successfully
+         */
+        if (rowId > 0) {
+            // successfully
+            Uri _uri = ContentUris.withAppendedId(contentURI, rowId);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        } else {
+            throw new SQLException("Failed to add a record into " + uri);
+        }
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(tableName);
+
+        switch (uriMatcher.match(uri)) {
+            case EMPLOYEES:
+                qb.setProjectionMap(employeesMap);
+                break;
+
+            case EMPLOYEE_ID:
+                qb.appendWhere( employeeId + "=" + uri.getPathSegments().get(1));
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        if (sortOrder == null || sortOrder == ""){
+            /**
+             * By default sort on employees  names
+             */
+            sortOrder = employeeName;
+        }
+
+        Cursor c = qb.query(employeeDb,	projection,	selection, selectionArgs,null, null, sortOrder);
+
+        /**
+         * register to watch a content URI for changes
+         */
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return c;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        int count = 0;
+
+        switch (uriMatcher.match(uri)){
+            case EMPLOYEES:
+                count = employeeDb.update(tableName, values, selection, selectionArgs);
+                break;
+
+            case EMPLOYEE_ID:
+                count = employeeDb.update(tableName, values,employeeId + " = " + uri.getPathSegments().get(1) +
+                        (!TextUtils.isEmpty(selection) ? " AND (" +selection + ')' : ""), selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri );
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
+    }
+}
---
+++ app/src/main/java/com/smile/contentprovidertest/MainActivity.java
@@ -0,0 +1,85 @@
+package com.smile.contentprovidertest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickAddName(View view) {
+        // Add a new employee record
+        ContentValues values = new ContentValues();
+
+        values.put(EmployeeContentProvider.employeeName,
+                ((EditText)findViewById(R.id.editText1)).getText().toString());
+
+        values.put(EmployeeContentProvider.employeePhone,
+                ((EditText)findViewById(R.id.editText2)).getText().toString());
+
+        Uri uri = getContentResolver().insert(
+                EmployeeContentProvider.contentURI, values);
+
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickRetrieveEmployees(View view) {
+
+        // Retrieve  employee records
+        // String URL = "content://com.smile.provider.SmileCompany/employees";
+        String URL = EmployeeContentProvider.URL;
+
+        Uri employees = Uri.parse(URL);
+        Cursor c = getContentResolver().query(employees, null, null, null, "name");
+
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(EmployeeContentProvider.employeeId)) +
+                                ", " +  c.getString(c.getColumnIndex( EmployeeContentProvider.employeeName)) +
+                                ", " + c.getString(c.getColumnIndex( EmployeeContentProvider.employeePhone)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+
+        c.close();
+    }
+}
